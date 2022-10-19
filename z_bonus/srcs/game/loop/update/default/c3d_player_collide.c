ed0cf4cd5b654a9b609b81abea05d76a4da97a7e
Pierre-yves PAJOT <ppajot@e2r4p8.clusters.42paris.fr>
2022-10-19 21:12:35 +0200

    collision semi fixed

--- z_bonus/srcs/game/loop/update/default/c3d_player_collide.c
+++ z_bonus/srcs/game/loop/update/default/c3d_player_collide.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   c3d_player_collide.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lgiband <lgiband@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ppajot <ppajot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/30 11:42:39 by lgiband           #+#    #+#             */
-/*   Updated: 2022/10/19 00:04:13 by lgiband          ###   ########.fr       */
+/*   Updated: 2022/10/19 21:06:40 by ppajot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,30 +30,31 @@ double	get_wall_angle(char c)
 	return (0);
 }
 
-int	slide(t_game *game, t_wall wall, double limit, t_coord mov)
+int	slide(t_game *game, t_wall wall, t_coord mov)
 {
 	t_coord	new_mov;
 
 	new_mov.x = 0;
 	new_mov.y = 0;
+	wall.dist = wall.dist * dabs(cos(get_wall_angle(wall.face) - game->player.plane.value));
 	if (wall.face == 'N' || wall.face == 'S')
-		new_mov.x = mov.x * limit / (game->player.speed * game->delay);
+		new_mov.x = mov.x * dabs(cos(get_wall_angle(wall.face) - game->player.plane.value));
 	else
-		new_mov.y = mov.y * limit / (game->player.speed * game->delay);
+		new_mov.y = mov.y * dabs(cos(get_wall_angle(wall.face) - game->player.plane.value));
 	check_collide(game, new_mov);
 	return (0);
 }
 
 int	apply_collide(t_game *game, t_wall wall, t_coord mov)
 {
-	if (wall.dist > norm((double)mov.x, (double)mov.y)
+	if (wall.dist > norm(mov.x, mov.y)
 		&& wall.dist > 3 * VIEW_WIDTH / 2)
 	{
 		game->player.pos.x += mov.x;
-		game->player.pos.y += mov.y;
+		game->player.pos.y += mov.y;	
 	}
 	else
-		slide (game, wall, wall.dist, mov);
+		slide (game, wall, mov);
 	return (0);
 }
 
@@ -63,8 +64,6 @@ int	get_new_wall(t_game *game, t_vector player, t_wall *wall)
 	wall->dist = 0;
 	wall->dist_from_start = 0;
 	intersect_wall(game, player, wall);
-	wall->dist = wall->dist * dabs(cos(get_wall_angle(wall->face)
-				+ M_PI / 2 - player.angle.value));
 	return (0);
 }
 
@@ -76,10 +75,6 @@ int	check_collide(t_game *game, t_coord mov)
 
 	if (mov.x == 0 && mov.y == 0)
 		return (0);
-	player.x = game->player.pos.x + VIEW_WIDTH
-		* sin(game->player.plane.value) / 2;
-	player.y = game->player.pos.y + VIEW_WIDTH
-		* cos(game->player.plane.value) / 2;
 	if (mov.y > 0)
 		player.angle.value = 2 * M_PI
 			- acos(mov.x / norm((double)mov.x, (double)mov.y));
@@ -88,11 +83,15 @@ int	check_collide(t_game *game, t_coord mov)
 	player.angle.cos = cos(player.angle.value);
 	player.angle.sin = sin(player.angle.value);
 	player.angle.tan = player.angle.sin / player.angle.cos;
+	player.x = game->player.pos.x + mov.x + VIEW_WIDTH
+		* player.angle.sin / 2;
+	player.y = game->player.pos.y + mov.y + VIEW_WIDTH
+		* player.angle.cos / 2;
 	get_new_wall(game, player, &wall);
-	player.x = game->player.pos.x - VIEW_WIDTH
-		* sin(game->player.plane.value) / 2;
-	player.y = game->player.pos.y - VIEW_WIDTH
-		* cos(game->player.plane.value) / 2;
+	player.x = player.x - VIEW_WIDTH
+		* sin(game->player.plane.value);
+	player.y = player.y - VIEW_WIDTH
+		* cos(game->player.plane.value);
 	get_new_wall(game, player, &wall2);
 	if (min(wall.dist, wall2.dist) == wall2.dist)
 	{
